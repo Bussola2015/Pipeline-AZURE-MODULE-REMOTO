c8212a9e8da222603f27a10a9a6991fb5e17a10a
Valdir Bussola Junior <valdirbussola@gmail.com>
2026-02-26 17:00:49 -0300

    ajuste main.tf versão plugins

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,6 +8,16 @@ terraform {
       #version = "4.58.0"
       version = ">= 3.0.0"
     }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    
   }
 
   backend "azurerm" {
